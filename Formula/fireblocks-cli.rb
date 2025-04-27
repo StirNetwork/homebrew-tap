9251ed4c0ba716888e4186ed3d45f7a5df5c63ef
Shohei KAMON <cameong@stir.network>
2025-04-27 17:26:02 +0800

    update fireblocks-cli version

--- Formula/fireblocks-cli.rb
+++ Formula/fireblocks-cli.rb
@@ -1,14 +1,14 @@
 class FireblocksCli < Formula
   desc "An unofficial CLI for managing Fireblocks services"
   homepage "https://github.com/StirNetwork/fireblocks-cli"
-  url "https://github.com/StirNetwork/fireblocks-cli/releases/download/v0.1.8/fireblocks_cli-0.1.8.tar.gz"
-  sha256 "42b60747ef24297ecb499fc1beceeee1cbb76e7e07d536284b96444f74cc6c14"
+  url "https://github.com/StirNetwork/fireblocks-cli/releases/download/v0.1.9-alpha%2B5/fireblocks_cli-v0.1.9-alpha+5-macos-x86_64.tar.gz"
+  sha256 "aaf95b8d96eb0b0a60819bbd4fd97ecb746283d1206a81a0dc9d5deb05f5c010"
   license "MPL-2.0"
 
   depends_on "python@3.11"
 
   def install
-    bin.install "fireblocks_cli-0.1.8/fireblocks-cli"
+    bin.install "fireblocks-cli-v0.1.9-alpha+5-x86_64/fireblocks-cli"
   end
 
   test do
