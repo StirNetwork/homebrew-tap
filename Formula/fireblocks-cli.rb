2a702ee29daea228b1daad43de72bff4b61d006a
Shohei KAMON <cameong@stir.network>
2025-04-28 01:32:22 +0800

    update build process
    Signed-off-by: Shohei KAMON <cameong@stir.network>

--- Formula/fireblocks-cli.rb
+++ Formula/fireblocks-cli.rb
@@ -1,14 +1,14 @@
 class FireblocksCli < Formula
   desc "An unofficial CLI for managing Fireblocks services"
   homepage "https://github.com/StirNetwork/fireblocks-cli"
-  url "https://github.com/StirNetwork/fireblocks-cli/releases/download/v0.1.9-alpha%2B5/fireblocks_cli-v0.1.9-alpha+5-macos-x86_64.tar.gz"
+  url "https://github.com/StirNetwork/fireblocks-cli/releases/download/v0.1.9-alpha%2B9/fireblocks_cli-0.1.9.tar.gz"
   sha256 "aaf95b8d96eb0b0a60819bbd4fd97ecb746283d1206a81a0dc9d5deb05f5c010"
   license "MPL-2.0"
 
   depends_on "python@3.11"
 
   def install
-    bin.install "fireblocks-cli-v0.1.9-alpha+5-x86_64/fireblocks-cli"
+    virtualenv_install_with_resources
   end
 
   test do
