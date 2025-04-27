c7a90b6c530867dc5c7b2aa936ded8e6ddd4b2cd
Shohei KAMON <cameong@stir.network>
2025-04-27 14:53:35 +0800

    initial commit
    Signed-off-by: Shohei KAMON <cameong@stir.network>

---
+++ Formula/fireblocks-cli.rb
@@ -0,0 +1,18 @@
+class FireblocksCli < Formula
+  desc "An unofficial CLI for managing Fireblocks services"
+  homepage "https://github.com/StirNetwork/fireblocks-cli"
+  url "https://github.com/StirNetwork/fireblocks-cli/releases/download/v0.1.8/fireblocks_cli-0.1.8.tar.gz"
+  sha256 "42b60747ef24297ecb499fc1beceeee1cbb76e7e07d536284b96444f74cc6c14"
+  license "MPL-2.0"
+
+  depends_on "python@3.11"
+
+  def install
+    bin.install "fireblocks-cli"
+  end
+
+  test do
+    system "#{bin}/fireblocks-cli", "--help"
+  end
+end
+
